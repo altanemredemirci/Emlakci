ee36181d5faf2de2142449a9bc0a24981e5b379c
Altan Emre <altan_emre_1989@hotmail.com>
2024-04-21 10:50:15 +0300

    Index içinde _HomeProductListComponentPartial ve _HomeDiscountOfDayComponentPartial yerdeğiştir
    _HomeProductsListComponentPartial/Default revize
    Bootstrap den NavTabs al ve yapıyı değiştir
    _PartialProducts yaz

---
+++ Emlakci.WEBUI/ViewComponents/Home/_HomeDiscountOfDayViewComponentPartial.cs
@@ -0,0 +1,38 @@
+﻿using Emlakci.BLL.Abstract;
+using Emlakci.WEBUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Emlakci.WEBUI.ViewComponents.Home
+{
+    public class _HomeDiscountOfDayViewComponentPartial : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public _HomeDiscountOfDayViewComponentPartial(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            List<ResultProductModel> resultProductModels = new List<ResultProductModel>();
+
+            foreach (var item in _productService.GetAll())
+            {
+                resultProductModels.Add(new ResultProductModel()
+                {
+                    Id = item.Id,
+                    Title = item.Title,
+                    Price = item.Price,
+                    City = item.City,
+                    District = item.District,
+                    Address = item.Address,
+                    CategoryName = item.Category.Name,
+                    CoverImage = item.CoverImage,
+                    Type = item.Type
+                });
+            }
+
+            return View(resultProductModels);
+        }
+    }
+}
