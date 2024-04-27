11993b0e26ecc712f6b5ad6d84fe7d356ef5617a
Altan Emre <altan_emre_1989@hotmail.com>
2024-04-27 11:11:46 +0300

    Admin Template WEBUI'a uyarla AdminController\Index _AdminLayoutHeadComponentPartial.cs yaz _AdminLayoutPreloaderComponentPartial.cs yaz _AdminLayoutNavbarComponentPartial.cs yaz _AdminLayoutSidebarComponentPartial.cs yaz _AdminLayoutContentHeaderComponentPartial.cs yaz _AdminLayoutScriptComponentPartial.cs yaz Default sayfalarını yaz

---
+++ Emlakci.WEBUI/ViewComponents/Admin/_AdminLayoutContentHeaderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Emlakci.WEBUI.ViewComponents.Admin
+{
+    public class _AdminLayoutContentHeaderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Emlakci.WEBUI/ViewComponents/Admin/_AdminLayoutNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Emlakci.WEBUI.ViewComponents.Admin
+{
+    public class _AdminLayoutNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
