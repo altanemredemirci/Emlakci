b9c2b63ff7918ee7f076f43ce6a66f33a95ccdae
Altan Emre <altan_emre_1989@hotmail.com>
2024-04-27 12:25:36 +0300

    AdminLTE Template _LayoutADmin.cshtml Admin Controller/Index() Index.html Models/CategoryModel CategoryController/Index() - CategoryListeleme Index.cshtml

---
+++ Emlakci.WEBUI/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Emlakci.BLL.Abstract;
+using Emlakci.Entity;
+using Emlakci.WEBUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Emlakci.WEBUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            var models = new List<CategoryModel>();
+
+            foreach (var item in _categoryService.GetAll())
+            {
+                models.Add(new CategoryModel()
+                {
+                    Id = item.Id,
+                    Icon = item.Icon,
+                    Name = item.Name,
+                    Status = item.Status
+                });
+            }
+            return View(models);
+        }
+    }
+}
--- Emlakci.WEBUI/Program.cs
+++ Emlakci.WEBUI/Program.cs
@@ -56,7 +56,7 @@ namespace Emlakci.WEBUI
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Admin}/{action=Index}/{id?}");
 
             app.Run();
         }
